7fdf6220131cf86a3c9f2b955cd1a488a6ef790b
Luiz Quirino <luizfpq@gmail.com>
2022-09-14 13:25:09 -0300

    refactor buscaPlacaAntiga

--- classes/ListaDuplamenteEncadeada.py
+++ classes/ListaDuplamenteEncadeada.py
@@ -151,19 +151,15 @@ class ListaDuplamenteEncadeada:
         return celula
 
     def buscaPlacaAntiga(self, placa):
-        
         letras = {
-            "0" : "A", "1" : "B", "2" : "C", "3" : "D", "4" : "E", "5" : "F", "6" : "G", "7" : "H", "8" : "I", "9" : "J",
+            "0" : "A", "1" : "B", "2" : "C", "3" : "D", "4" : "E",
+            "5" : "F", "6" : "G", "7" : "H", "8" : "I", "9" : "J",
             }
-        
-        
-        
         try:
             placa = list(placa)
             letra = placa.pop(4)
             placa.insert(4, letras.get(letra))
             placa = ''.join(map(str, placa))
-        
             return placa
         except Exception:
             return
